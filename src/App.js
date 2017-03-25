4140334c7e2a6f4fe32fad075bd1f9ff6a1ac81d
Mariusz Rak <rak.m.rak@gmail.com>
2017-03-25 17:42:19 +0100

    initial commit at end of submod8.3

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DevTools from './DevTools';
+
+const App = () => {
+  return (
+    <div className="App">
+      <DevTools />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import App from './App';
+import { getCountries } from './actions/actions-countries'
+
+render(
+  <Provider  store={store}>
+    <App />
+  </Provider>,
+    document.getElementById('root')
+);
+
+store.dispatch(getCountries());
